5d7e9f6a0a5dac40094c0185dd9643a4b3b467e9
sale7x <30891295+sale7x@users.noreply.github.com>
2023-03-24 20:03:58 +0300

    Workable perfect

--- delete_car.php
+++ delete_car.php
@@ -1,17 +1,26 @@
 <?php
+
 // Get the ID of the car to be deleted from the request parameters
 $id = $_POST['id'];
 
 // Connect to the database
-$host = '127.0.0.1';
+$servername = '127.0.0.1';
 $dbname = 'carreminder';
 $username = 'root';
 $password = '';
-$db = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+// Create database connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check for errors in connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+
 
 // Prepare the SQL statement to delete the car record with the given ID
-$stmt = $db->prepare('DELETE FROM cars WHERE id = :id');
-$stmt->bindParam(':id', $id);
+$stmt = $conn->prepare('DELETE FROM cars WHERE id = ?');
+$stmt->bind_param('i', $id);
 $stmt->execute();
 
 // Return a success response
